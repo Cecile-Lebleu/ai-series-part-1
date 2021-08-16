5704043fb1bd70f0508f665f8385c6fce1cf0826
Cecile-Lebleu <czl.lebleu@gmail.com>
2021-08-16 17:59:20 -0600

    Up to video 4

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,12 @@
+using UnityEngine.AI;
+
+public class Enemy : PoolableObject {
+    public EnemyMovement Movement;
+    public NavMeshAgent Agent;
+    public int Health = 100;
+
+    public override void OnDisable() {
+        base.OnDisable();
+        Agent.enabled = false;
+    }
+}
